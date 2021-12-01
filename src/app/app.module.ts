b4108bcb6017f1b397158968f7f7a4b35fecc8da
esthervsanchez <ESTHER.SANCHEZ@newhorizons.edu.do>
2021-12-01 12:40:25 +0000

    component2 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { ButtomComponent } from './buttom/buttom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    ButtomComponent
   ],
   imports: [
     BrowserModule,
